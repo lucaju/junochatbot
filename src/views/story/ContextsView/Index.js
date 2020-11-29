e4e6a3414803517db3863a238a911b72435153a7
Luciano Frizzera <lucaju@gmail.com>
2020-11-28 20:57:03 -0500

    feat: 🎸 Introduce Context View

--- src/views/story/ContextsView/Index.js
+++ src/views/story/ContextsView/Index.js
@@ -1,37 +1,69 @@
 import {
-    Box,
-    CircularProgress,
-    Container,
-    makeStyles
-  } from '@material-ui/core';
-  import React, { useEffect, useState } from 'react';
-//   import { useNavigate } from 'react-router-dom';
-  import Page from 'src/components/Page';
-  import { useApp } from 'src/overmind';
-  
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      backgroundColor: theme.palette.background.dark,
-      minHeight: '100%',
-      paddingBottom: theme.spacing(3),
-      paddingTop: theme.spacing(3),
-    },
-  }));
-  
-  const title = 'Contexts';
-  
-  const UsersListView = () => {
-    const classes = useStyles();
-    const { state, actions } = useApp();
-  
-    return (
-      <Page className={classes.root} title={title}>
-        <Container maxWidth={false}>
-        Contexts
-        </Container>
-      </Page>
-    );
-  };
-  
-  export default UsersListView;
-  
+  Box,
+  CircularProgress,
+  Container,
+  makeStyles,
+} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Page from 'src/components/Page';
+import { useApp } from 'src/overmind';
+import Collection from './Collection';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3),
+  },
+}));
+
+const title = 'Chat Stories';
+
+const ContextView = () => {
+  const classes = useStyles();
+  const { state, actions } = useApp();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (!state.story.currentStory.id) navigate('/', { replace: true });
+    const getCollection = async () => {
+      await actions.intentContext.getCollection();
+      setIsLoading(false);
+    };
+    getCollection();
+    return () => {};
+  }, []);
+
+  return (
+    <Page className={classes.root} title={title}>
+      <Container maxWidth={false}>
+        {isLoading && (
+          <Box
+            display="flex"
+            height="100%"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <CircularProgress
+              className={classes.spinner}
+              size={60}
+              thickness={4}
+            />
+          </Box>
+        )}
+        {!isLoading && (
+          <>
+            <Box mt={3}>
+              <Collection contexts={state.intentContext.collection} />
+            </Box>
+          </>
+        )}
+      </Container>
+    </Page>
+  );
+};
+
+export default ContextView;
