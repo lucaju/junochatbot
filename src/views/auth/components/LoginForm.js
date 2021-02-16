eabfbad87cc6257b2bbaa640038891784b2d8271
Luciano Frizzera <lucaju@gmail.com>
2021-02-16 17:29:39 -0500

    feat: 🎸 Add options to show password

--- src/views/auth/components/LoginForm.js
+++ src/views/auth/components/LoginForm.js
@@ -2,17 +2,24 @@ import {
   Box,
   Button,
   CircularProgress,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
   makeStyles,
   TextField,
 } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import { Formik } from 'formik';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import * as Yup from 'yup';
 
-const useStyles = makeStyles((theme) => ({
-  submitButton: { color: '#FFFFFF' },
-  buttonProgress: {
+const useStyles = makeStyles(({ palette }) => ({
+  submitButton: { color: palette.common.white },
+  progress: {
     position: 'absolute',
     marginLeft: '45%',
   },
@@ -28,6 +35,11 @@ const formValidation = Yup.object().shape({
 
 const LoginForm = ({ authenticate, setIsAuthenticating }) => {
   const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = (event) => event.preventDefault();
+
   return (
     <Formik
       initialValues={{ email: '', password: '' }}
@@ -60,36 +72,44 @@ const LoginForm = ({ authenticate, setIsAuthenticating }) => {
             type="email"
             value={values.email}
           />
-          <TextField
-            autoComplete="current-password"
-            error={Boolean(touched.password && errors.password)}
-            fullWidth
-            helperText={touched.password && errors.password}
-            label="Password"
-            margin="normal"
-            name="password"
-            onBlur={handleBlur}
-            onChange={handleChange}
-            type="password"
-            value={values.password}
-          />
+          <FormControl fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    edge="end"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              error={Boolean(touched.password && errors.password)}
+              id="password"
+              name="password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              type={showPassword ? 'text' : 'password'}
+              value={values.password}
+            />
+          </FormControl>
           <Box my={2}>
             <Button
+              classes={{ containedPrimary: classes.submitButton }}
               color="primary"
               disabled={isSubmitting}
+              disableElevation
               fullWidth
               size="large"
               type="submit"
               variant="contained"
-              disableElevation
-              classes={{ containedPrimary: classes.submitButton }}
             >
               Sign in
               {isSubmitting && (
-                <CircularProgress
-                  size={24}
-                  className={classes.buttonProgress}
-                />
+                <CircularProgress className={classes.progress} size={24} />
               )}
             </Button>
           </Box>
--- src/views/auth/components/ResetPasswordForm.js
+++ src/views/auth/components/ResetPasswordForm.js
@@ -2,28 +2,39 @@ import {
   Box,
   Button,
   CircularProgress,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
   makeStyles,
-  TextField,
 } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import { Formik } from 'formik';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import * as Yup from 'yup';
 
 const useStyles = makeStyles(({ palette }) => ({
   submitButton: { color: palette.common.white },
-  buttonProgress: {
+  progress: {
     position: 'absolute',
     marginLeft: '45%',
   },
 }));
 
 const formValidation = Yup.object().shape({
-  password: Yup.string().max(255).required('Password is required'),
+  password: Yup.string().min(5).max(255).required('Password is required'),
 });
 
 const ResetPasswordForm = ({ resetPassword }) => {
   const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = (event) => event.preventDefault();
+
   return (
     <Formik
       initialValues={{ password: '' }}
@@ -42,19 +53,30 @@ const ResetPasswordForm = ({ resetPassword }) => {
         values,
       }) => (
         <form onSubmit={handleSubmit}>
-          <TextField
-            autoComplete="current-password"
-            error={Boolean(touched.password && errors.password)}
-            fullWidth
-            helperText={touched.password && errors.password}
-            label="New Password"
-            margin="normal"
-            name="password"
-            onBlur={handleBlur}
-            onChange={handleChange}
-            type="password"
-            value={values.password}
-          />
+          <FormControl fullWidth>
+            <InputLabel htmlFor="password">New Password</InputLabel>
+            <Input
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    edge="end"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              error={Boolean(touched.password && errors.password)}
+              id="password"
+              name="password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              type={showPassword ? 'text' : 'password'}
+              value={values.password}
+            />
+          </FormControl>
           <Box my={2}>
             <Button
               classes={{ containedPrimary: classes.submitButton }}
@@ -68,10 +90,7 @@ const ResetPasswordForm = ({ resetPassword }) => {
             >
               Submit
               {isSubmitting && (
-                <CircularProgress
-                  className={classes.buttonProgress}
-                  size={24}
-                />
+                <CircularProgress className={classes.progress} size={24} />
               )}
             </Button>
           </Box>
