fcdfdd5d1baee3bfc910e3e379d1a42aee1ea65b
Luciano Frizzera <lucaju@gmail.com>
2021-02-20 17:57:07 -0500

    refactor: 💡 update mockup data

--- src/mockData/video.js
+++ src/mockData/video.js
@@ -2,12 +2,10 @@ export const dataVideoCollection = [
   {
     id: 1,
     source: 'https://www.youtube.com/watch?v=2MQx0SXLCcE',
-    provider: 'youtube',
     image: 'image1.jpg',
     title: 'After Life2',
     author: 'Luciano Frizzera',
     year: 2002,
-    genre: 'Intervew',
     description: 'Just a video',
     tags: [
       { id: 13, name: 'Person' },
