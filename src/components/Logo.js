7946861fd6dcf55938035cb1a481c41207c4af3c
Luciano Frizzera <lucaju@gmail.com>
2021-02-22 22:46:49 -0500

    fix: 🐛 Change logo file

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,16 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { useApp } from 'src/overmind';
 
-const Logo = ({ type, ...props }) => {
-  const { state } = useApp();
-
-  const path = '/assets/logo';
-  const file = `logo_${type}.png`;
-  // const file = `logo_${type}_${state.ui.darkMode}.png`;
-
-  return <img src={`${path}/${file}`} alt="Juno Chatbot" {...props} />;
-};
+const Logo = ({ type, ...props }) => (
+  <img src={`/assets/logo/logo_${type}.png`} alt="Juno Chatbot" {...props} />
+);
 
 Logo.defaultProps = {
   type: 'full',
