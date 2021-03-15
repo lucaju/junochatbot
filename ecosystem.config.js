f9fc65de4fc934bc09d17fd31f0e9cb813b3a160
Luciano Frizzera <lucaju@gmail.com>
2021-03-14 23:22:20 -0400

    ci: 🎡 set env variable

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,9 @@ module.exports = {
       script: './server/index.mjs',
       args: '--no-daemon',
       node_args: '--experimental-top-level-await',
+      env:  {
+        NODE_ENV: 'production',
+      }
     },
   ],
 };
