5271b2566440ca8fb1ccd329507c043b742fd55d
Luciano Frizzera <lucaju@gmail.com>
2021-05-28 17:57:27 -0400

    feat: 🎸 Intent response: add ability to lookup and include parameter

---
+++ src/views/story/ConversationView/details/responses/MenuParameters.tsx
@@ -0,0 +1,73 @@
+import { Menu, MenuItem } from '@material-ui/core';
+import React, { FC, useEffect, useState } from 'react';
+import { getCursorXY, useCallParameters } from './parameters';
+
+interface MenuParametersProps {
+  open: boolean;
+  target: HTMLElement | undefined;
+  inputElement: HTMLInputElement | undefined;
+  handleClick: (value: string, position: number) => void;
+  handleClose: () => void;
+}
+
+type MenuAnchorPositionType = {
+  top: number;
+  left: number;
+};
+
+const MenuParameters: FC<MenuParametersProps> = ({
+  inputElement,
+  open,
+  target,
+  handleClick,
+  handleClose,
+}) => {
+  const params = useCallParameters();
+  const [charPosition, setCharPosition] = useState(0);
+  const [anchorPosition, setAnchorPosition] = useState<MenuAnchorPositionType>({ top: 0, left: 0 });
+
+  useEffect(() => {
+    if (open) setAnchorPosition(callAnchorPosition());
+    return () => {};
+  }, [open]);
+
+  const callAnchorPosition = () => {
+    if (!inputElement || !target) return { top: 0, left: 0 };
+
+    const rect = target.getBoundingClientRect();
+    const cursorPos = inputElement.selectionEnd ?? 0;
+    const cursor = getCursorXY(inputElement, cursorPos);
+    const margintop = 28;
+
+    setCharPosition(cursorPos);
+
+    return {
+      top: rect.top + margintop,
+      left: rect.left + cursor.x,
+    };
+  };
+
+  return (
+    <Menu
+      anchorPosition={anchorPosition}
+      anchorReference="anchorPosition"
+      open={open}
+      onClose={handleClose}
+    >
+      {params.map(({ value }) => (
+        <MenuItem
+          key={value}
+          value={value}
+          onClick={() => {
+            if (!value) return;
+            handleClick(value, charPosition);
+          }}
+        >
+          {value}
+        </MenuItem>
+      ))}
+    </Menu>
+  );
+};
+
+export default MenuParameters;
--- src/views/story/ConversationView/details/responses/TextMessageContent.tsx
+++ src/views/story/ConversationView/details/responses/TextMessageContent.tsx
@@ -1,6 +1,15 @@
 import { Box, IconButton, makeStyles, TextField, Zoom } from '@material-ui/core';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
-import React, { ChangeEvent, FC, FocusEvent, useEffect, useState } from 'react';
+import React, {
+  ChangeEvent,
+  FC,
+  FocusEvent,
+  KeyboardEvent,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import MenuParameters from './MenuParameters';
 
 interface TextMessageContentProps {
   index: number;
@@ -23,8 +32,11 @@ const TextMessageContent: FC<TextMessageContentProps> = ({
   handleUpdate,
 }) => {
   const classes = useStyles();
+  const TFref = useRef();
+  const [elTarget, setElTarget] = useState<HTMLElement>();
   const [hover, setHover] = useState(false);
   const [value, setValue] = useState(content);
+  const [showParameters, setShowParameters] = useState(false);
 
   useEffect(() => {
     setValue(content);
@@ -34,10 +46,34 @@ const TextMessageContent: FC<TextMessageContentProps> = ({
     setValue(event.currentTarget.value);
   };
 
+  const handleKeyUp = (event: KeyboardEvent<HTMLElement>) => {
+    if (event.key !== '$') return;
+    setElTarget(event.currentTarget);
+    setShowParameters(true);
+  };
+
   const handleBlur = (event: FocusEvent<HTMLInputElement>) => {
     const value = event.currentTarget.value;
     if (value === '') return handleRemove(index);
     handleUpdate(index, value);
+  };
+
+  const handleParamSelect = (param: string, position: number) => {
+    handleParamsClose();
+    if (!param) return;
+
+    const paramName = param.substring(1);
+    const partA = value.substring(0, position);
+    const partB = value.substring(position);
+    const newValue = `${partA}${paramName}${partB}`;
+
+    setValue(newValue);
+    setShowParameters(false);
+  };
+
+  const handleParamsClose = () => {
+    setShowParameters(false);
+    setElTarget(undefined);
   };
 
   return (
@@ -49,11 +85,20 @@ const TextMessageContent: FC<TextMessageContentProps> = ({
       onMouseLeave={() => setHover(false)}
     >
       <TextField
+        inputRef={TFref}
         className={classes.marginBottom}
         fullWidth
         onChange={handleChange}
         onBlur={handleBlur}
+        onKeyUp={handleKeyUp}
         value={value}
+      />
+      <MenuParameters
+        inputElement={TFref.current}
+        open={showParameters}
+        target={elTarget}
+        handleClick={handleParamSelect}
+        handleClose={handleParamsClose}
       />
       <Zoom in={removable && hover}>
         <IconButton
