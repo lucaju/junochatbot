60000554e2ad9c20184cce737a6acd462cc4148d
Luciano Frizzera <lucaju@gmail.com>
2021-06-26 23:51:17 -0400

    fix: 🐛 overmind api updates. Marterial-ui updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import React, { FC } from 'react';
 import { useRoutes } from 'react-router-dom';
 import Notification from './components/Notification';
-import { useApp } from './overmind';
+import { useAppState } from './overmind';
 import routes from './routes';
 import theme from './theme';
 import { useTracking } from './tracking';
 
 const App: FC = () => {
   const routing = useRoutes(routes);
-  const { state } = useApp();
+  const { ui } = useAppState();
   useTracking(process.env.GA_MEASUREMENT_ID);
 
   return (
-    <ThemeProvider theme={theme(state.ui.darkMode)}>
+    <ThemeProvider theme={theme(ui.darkMode)}>
       <CssBaseline />
       {routing}
       <Notification />
