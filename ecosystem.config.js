54c15e324aafe10fb462cdf44eebb95f5277a627
Luciano Frizzera <lucaju@gmail.com>
2021-07-13 16:15:10 -0400

    fix: 🐛 file extension

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'juno',
-      script: 'ts-node ./server/index.tjs',
+      script: 'ts-node ./server/index.ts',
       args: '--no-daemon',
       node_args: '--experimental-top-level-await',
       env: {
