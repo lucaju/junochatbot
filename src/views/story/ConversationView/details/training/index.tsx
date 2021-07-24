91435fad7492f1c1679b67001fc63ba41cbe98e9
Luciano Frizzera <lucaju@gmail.com>
2021-07-23 20:31:11 -0400

    fix: 🐛 training phrases: add explainer when fallback

--- src/views/story/ConversationView/details/training/index.tsx
+++ src/views/story/ConversationView/details/training/index.tsx
@@ -1,11 +1,12 @@
 import { Box, Button, Typography } from '@material-ui/core';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import { useActions } from '@src/overmind';
+import { useActions, useAppState } from '@src/overmind';
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import Collection from './Collection';
 
 const Training: FC = () => {
+  const { intents } = useAppState();
   const actions = useActions();
   const { t } = useTranslation(['intents']);
 
@@ -15,7 +16,9 @@ const Training: FC = () => {
     <Box>
       <Box display="flex" flexDirection="column" alignItems="center" my={1.5}>
         <Typography gutterBottom variant="caption">
-          Phrases you can expect from user, that will trigger the intent.
+          {intents.currentIntent?.isFallback
+            ? t('trainingFallbackExplainer')
+            : t('trainingExplainer')}
         </Typography>
         <Button color="primary" onClick={addNewPhrase} startIcon={<AddCircleOutlineIcon />}>
           {t('addPhrase')}
