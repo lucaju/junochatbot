70123aed645c717b576468e01bb56be187c4835b
Luciano Frizzera <lucaju@gmail.com>
2021-07-31 18:13:35 -0400

    fix: 🐛 response: scroll view when adding new responses

--- src/views/story/ConversationView/details/responses/index.tsx
+++ src/views/story/ConversationView/details/responses/index.tsx
@@ -1,20 +1,41 @@
 import { Box, Button } from '@material-ui/core';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import { useActions } from '@src/overmind';
-import React, { FC } from 'react';
+import { useActions, useAppState } from '@src/overmind';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Collection from './Collection';
 
 const Responses: FC = () => {
+  const {
+    intents: { currentIntent },
+  } = useAppState();
   const actions = useActions();
   const { t } = useTranslation();
+  const ref = useRef<HTMLDivElement>();
+  const [refCurrentHeight, setRefCurrentHeight] = useState(0);
+
+  // update div scroll
+  useEffect(() => {
+    if (!ref.current) return;
+    autoScroll();
+  }, [currentIntent?.messages, refCurrentHeight]);
+
+  const autoScroll = () => {
+    setTimeout(() => {
+      if (!ref.current) return;
+      if (refCurrentHeight > 0 && refCurrentHeight < ref.current.scrollHeight) {
+        ref.current.scrollTop = ref.current.scrollHeight;
+      }
+      setRefCurrentHeight(ref.current.scrollHeight);
+    }, 0);
+  };
 
   const addTextMessage = () => actions.intents.addTextMessage();
   const addVideoMessage = () => actions.intents.addVideoMessage();
   const addVideoTagMessage = () => actions.intents.addVideoTagMessage();
 
   return (
-    <Box>
+    <Box ref={ref} height="100%" sx={{ overflow: 'auto' }}>
       <Box display="flex" flexDirection="column" alignItems="center" my={1.5}>
         <Box display="flex" flexDirection="row" my={1.5}>
           <Button color="primary" onClick={addTextMessage} startIcon={<AddCircleOutlineIcon />}>
