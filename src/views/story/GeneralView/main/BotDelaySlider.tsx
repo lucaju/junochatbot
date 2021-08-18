d971cfe6ae2d843918db1e5c95ad615cea3751fe
Luciano Frizzera <lucaju@gmail.com>
2021-08-18 19:37:43 -0400

    refactor: 💡 Bot general: rephrase bot delay response from number to semantic
    Intead of using range from 0-180, use semantic indicators: immediately,
    slow, fast

--- src/views/story/GeneralView/main/BotDelaySlider.tsx
+++ src/views/story/GeneralView/main/BotDelaySlider.tsx
@@ -1,27 +1,27 @@
 import { Slider } from '@material-ui/core';
 import { useField } from 'formik';
 import React, { FC, SyntheticEvent, useState } from 'react';
-
-const marks = [
-  { value: 0, label: '0' },
-  { value: 80, label: '80' },
-  { value: 100, label: '100' },
-  { value: 120, label: '120' },
-  { value: 140, label: '140' },
-  { value: 160, label: '160' },
-  { value: 180, label: '180' },
-];
+import { useTranslation } from 'react-i18next';
 
 const BotDelaySlider: FC = () => {
+  const { t } = useTranslation();
   const [, meta, helpers] = useField('botDelay');
   const { value } = meta;
   const { setValue } = helpers;
-  const [sliderValue, setSliderValue] = useState(value);
+  const [sliderValue] = useState(value);
 
-  const onChangeCommitted = (
-    event: Event | SyntheticEvent<Element, Event>,
-    value: number | number[]
-  ) => {
+  //on characters per millisecond
+  const marks = [
+    { value: 0, label: t('common:immediately')},
+    { value: 80, label: t('common:slow') },
+    { value: 100, label: '' },
+    { value: 120, label: '' },
+    { value: 140, label: '' },
+    { value: 160, label: '' },
+    { value: 180, label: t('common:fast') },
+  ];
+
+  const onChangeCommitted = (event: Event | SyntheticEvent, value: number | number[]) => {
     if (Array.isArray(value)) return;
     setValue(value);
   };
@@ -34,7 +34,10 @@ const BotDelaySlider: FC = () => {
       marks={marks}
       max={180}
       step={null}
-      valueLabelDisplay="auto"
+      sx={{
+        ml: 2,
+        '& .MuiSlider-markLabel': { fontSize: '0.75rem' },
+      }}
     />
   );
 };
